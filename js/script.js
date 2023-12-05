a877cba201d41517407a0b6e5b20586f3aaa5095
Soumajit Das <Cykeek@proton.me>
2023-12-05 14:53:29 +0530

    Initial Upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',()=>{
+    const bignav = document.querySelector('.bignav')
+    const nav = document.querySelector('.navbar')
+    bignav.classList.add('active')
+    nav.classList.add('active')
+})
