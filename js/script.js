85889b3fa2b0517af6511715c9637f623d078706
Soumajit Das <Cykeek@proton.me>
2023-12-08 11:38:55 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-window.addEventListener('load',()=>{
-    const bignav = document.querySelector('.bignav')
-    const nav = document.querySelector('.navbar')
-    bignav.classList.add('active')
-    nav.classList.add('active')
-})
+const i=0;
+if (i===0){
+    window.addEventListener('load',()=>{
+        const bignav = document.querySelector('.bignav')
+        const nav = document.querySelector('.navbar')
+        bignav.classList.add('active')
+        nav.classList.add('active')
+        i=1
+    })
+}
